f7d3c79b709e805c857fffa8f157e2935937904a
GoldyRoger07 <latouche822@gmail.com>
2026-06-02 13:43:18 -0400

    test duxor config 2

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-    { path: '', loadComponent: ()=> import('./pages/home/home')  },
+    { path: '', pathMatch: 'full', loadComponent: ()=> import('./pages/home/home')  },
     { path: 'about-us', loadComponent: ()=> import('./pages/about/about')  },
 ];
