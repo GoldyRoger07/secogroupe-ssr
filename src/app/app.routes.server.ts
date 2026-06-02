7d25bb80c728ba3c1d3975a6b25169c1fea1fa83
GoldyRoger07 <latouche822@gmail.com>
2026-06-02 12:53:53 -0400

    test 1

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,7 +2,7 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   { path: '', renderMode: RenderMode.Server },
-  { path: 'about-us', renderMode: RenderMode.Server },
+  { path: 'about-us', renderMode: RenderMode.Prerender },
   {
     path: '**',
     renderMode: RenderMode.Prerender
